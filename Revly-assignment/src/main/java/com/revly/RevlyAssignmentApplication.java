c35c5456832557991d205bbb95ed759ce565c9e4
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-11-10 13:02:56 +0530

    revly assignment created

---
+++ Revly-assignment/src/main/java/com/revly/RevlyAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.revly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevlyAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevlyAssignmentApplication.class, args);
+	}
+
+}
